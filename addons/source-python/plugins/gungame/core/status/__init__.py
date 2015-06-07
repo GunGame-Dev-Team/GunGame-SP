7a5bb849993feec8553b890a81fb37178b7cb11b
Stephen Toon <satoon101@gmail.com>
2015-06-07 13:16:58 -0400

    Updated core.status

--- addons/source-python/plugins/gungame/core/status/__init__.py
+++ addons/source-python/plugins/gungame/core/status/__init__.py
@@ -1,52 +1,48 @@
 # ../gungame/core/status/__init__.py
 
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Python Imports
+#   Enum
+from enum import Enum
 
 # =============================================================================
 # >> CLASSES
 # =============================================================================
-class _StatusTypes(object):
-    '''Base Status values'''
+class GunGameStatusTypes(Enum):
+    """Base Status values"""
 
-    @property
-    def INACTIVE(self):
-        '''False value for statuses'''
-        return False
-
-    @property
-    def ACTIVE(self):
-        '''True value for statuses'''
-        return True
-
-# Get the _StatusTypes instance
-StatusTypes = _StatusTypes()
+    INACTIVE = False
+    ACTIVE = True
 
 
 class _GunGameStatus(object):
-    '''Stores statuses for GunGame'''
+    """Stores statuses for GunGame"""
 
     # Set the base attributes all to False to start
-    Match = Loading = Round = False
+    match = loading = round = GunGameStatusTypes.INACTIVE
 
     def __setattr__(self, attribute, value):
-        '''Override __setattr__ to only allow proper
-            attributes to be set to proper values'''
-
+        """Override __setattr__ to only allow proper
+            attributes to be set to proper values"""
         # Is the given attribute a GunGame attribute?
         if not hasattr(self, attribute):
 
             # If not, raise an error
-            raise AttributeError('Cannot set attribute "%s"' % attribute)
+            raise AttributeError(
+                'Cannot set attribute "{0}"'.format(attribute))
 
         # Is the given value a boolean?
-        if not isinstance(value, bool):
+        if type(value) is not GunGameStatusTypes:
 
             # If not, raise an error
             raise ValueError(
-                'GunGameStatus attributes can only be ' +
-                'set to booleans, not "%s"' % type(value).__name__)
+                'GunGameStatus attributes can only be set using ' +
+                'GunGameStatusTypes, not "{0}"'.format(type(value).__name__))
 
         # Set the attribute to the given value
         super(_GunGameStatus, self).__setattr__(attribute, value)
 
 # Get the _GunGameStatus instance
-GunGameStatus = _GunGameStatus()
+gungame_status = _GunGameStatus()
--- addons/source-python/plugins/gungame/core/status/events.py
+++ addons/source-python/plugins/gungame/core/status/events.py
@@ -9,8 +9,8 @@ from events import Event
 
 # GunGame Imports
 #   Status
-from gungame.core.status import GunGameStatus
-from gungame.core.status import StatusTypes
+from gungame.core.status import gungame_status
+from gungame.core.status import GunGameStatusTypes
 
 
 # =============================================================================
@@ -18,31 +18,31 @@ from gungame.core.status import StatusTypes
 # =============================================================================
 @Event
 def gg_win(game_event):
-    '''Set match status to inactive when someone has won the match'''
-    GunGameStatus.Match = StatusTypes.INACTIVE
+    """Set match status to inactive when someone has won the match"""
+    gungame_status.match = GunGameStatusTypes.INACTIVE
 
 
 @Event
 def gg_match_end(game_event):
-    '''
+    """
         Set match status to inactive when the match has ended without a winner
-    '''
-    GunGameStatus.Match = StatusTypes.INACTIVE
+    """
+    gungame_status.match = GunGameStatusTypes.INACTIVE
 
 
 @Event
 def gg_start(game_event):
-    '''Set match status to active when the match starts'''
-    GunGameStatus.Match = StatusTypes.ACTIVE
+    """Set match status to active when the match starts"""
+    gungame_status.match = GunGameStatusTypes.ACTIVE
 
 
 @Event
 def round_start(game_event):
-    '''Set round status to active when the round starts'''
-    GunGameStatus.Round = StatusTypes.ACTIVE
+    """Set round status to active when the round starts"""
+    gungame_status.round = GunGameStatusTypes.ACTIVE
 
 
 @Event
 def round_end(game_event):
-    '''Set round status to inactive when the round ends'''
-    GunGameStatus.Round = StatusTypes.INACTIVE
+    """Set round status to inactive when the round ends"""
+    gungame_status.round = GunGameStatusTypes.INACTIVE
