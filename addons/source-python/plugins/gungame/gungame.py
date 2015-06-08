57baeb10cbd1d44266a596c289dfe5ae5e059cf2
Stephen Toon <satoon101@gmail.com>
2015-06-07 20:12:58 -0400

    Now calls gg_start event to start the match.
    Moved events and listeners out of gungame.py and into listeners.py.

--- addons/source-python/plugins/gungame/gungame.py
+++ addons/source-python/plugins/gungame/gungame.py
@@ -3,14 +3,14 @@
 """"""
 
 from cvars.tags import sv_tags
-from events import Event
 from filters.entities import EntityIter
-from listeners import LevelShutdown
+from listeners.tick import tick_delays
 
 from gungame.info import info
 from gungame.core.config.manager import config_manager
+from gungame.core.events.included.match import GG_Start
 from gungame.core.events.storage import gg_resource_list
-from gungame.core.players.dictionary import player_dictionary
+from gungame.core.status import GunGameStatus
 from gungame.core.status import gungame_status
 from gungame.core.weapons.manager import weapon_order_manager
 
@@ -44,6 +44,15 @@ def load():
     # Add gungame to sv_tags
     sv_tags.add(info.basename)
 
+    # Import the listeners/events
+    import gungame.listeners
+
+    # Import status events
+    import gungame.core.status.events
+
+    # Wait 1 tick to see if gg_start should be called
+    tick_delays.delay(0, start_match)
+
 
 def unload():
     """Clean up GunGame."""
@@ -65,111 +74,7 @@ def unload():
         entity.enable()
 
 
-@Event
-def player_spawn(game_event):
-    """Give the player their weapon."""
-    # Is GunGame active?
-    if not gungame_status.match:
+def start_match():
+    if gungame_status.match is not GunGameStatus.INACTIVE:
         return
-
-    # Get the userid of the player
-    userid = game_event.get_int('userid')
-
-    # Use try/except to get the player's instance
-    try:
-        player = player_dictionary[userid]
-    except ValueError:
-        return
-
-    # Verify that the player is on a team
-    if player.team < 2:
-        return
-
-    # Give player their current weapon
-    player.give_level_weapon()
-
-    if player.is_fake_client():
-        return
-
-    from messages import SayText2
-    SayText2(message='{0} - {1}/{2}'.format(
-        player.level, player.multikill, player.level_multikill)).send(
-            player.index)
-
-
-@Event
-def player_death(game_event):
-    """Award the killer if necessary."""
-    # Is GunGame active?
-    if not gungame_status.match:
-        return
-
-    # Is the round active or should kills after the round count?
-    if not gungame_status.round and not gg_count_after_round.get_int():
-        return
-
-    # Get the victim
-    victim = game_event.get_int('userid')
-
-    # Get the attacker
-    attacker = game_event.get_int('attacker')
-
-    # Was this a suicide?
-    if attacker in (victim, 0):
-        return
-
-    # Get the victim's instance
-    vplayer = player_dictionary[victim]
-
-    # Get the attacker's instance
-    aplayer = player_dictionary[attacker]
-
-    # Was this a team-kill?
-    if vplayer.team == aplayer.team:
-        return
-
-    # Did the killer kill using their level's weapon?
-    if game_event.get_string('weapon') != aplayer.level_weapon:
-        return
-
-    # Does the player need leveled up?
-    if aplayer.multikill >= aplayer.level_multikill -1:
-
-        print('leveling up')
-
-        # Was this a winning kill?
-        if aplayer.level == weapon_order_manager.active.max_levels:
-            print('YOU WON!!')
-            # Player wins!!!
-            return
-
-        # Increase the killer's level
-        aplayer.level += 1
-
-        # Reset the killer's multikill
-        aplayer.multikill = 0
-
-        print('LEVEL', aplayer.level)
-
-        # No need to go further
-        return
-
-    print('increase multikill')
-    # Increase the killer's multikill
-    aplayer.multikill += 1
-
-
-@Event
-def player_disconnect(game_event):
-    player_dictionary.safe_remove(game_event.get_int('userid'))
-
-
-@Event
-def round_start(game_event):
-    for entity in EntityIter('func_buyzone', return_types='entity'):
-        entity.disable()
-
-
-@LevelShutdown
-def level_shutdown():
-    player_dictionary.clear()
+    GG_Start().fire()
---
+++ addons/source-python/plugins/gungame/listeners.py
@@ -0,0 +1,118 @@
+from entities.entity import Entity
+from events import Event
+from filters.entities import EntityIter
+from listeners import LevelShutdown
+
+from gungame.core.players.dictionary import player_dictionary
+from gungame.core.status import GunGameStatus
+from gungame.core.status import gungame_status
+from gungame.core.weapons.manager import weapon_order_manager
+
+
+@Event
+def player_spawn(game_event):
+    """Give the player their weapon."""
+    # Is GunGame active?
+    if gungame_status.match is not GunGameStatus.ACTIVE:
+        return
+
+    # Get the userid of the player
+    userid = game_event.get_int('userid')
+
+    # Use try/except to get the player's instance
+    try:
+        player = player_dictionary[userid]
+    except ValueError:
+        return
+
+    # Verify that the player is on a team
+    if player.team < 2:
+        return
+
+    # Give player their current weapon
+    player.give_level_weapon()
+
+    if player.is_fake_client():
+        return
+
+    from messages import SayText2
+    SayText2(message='{0} - {1}/{2}'.format(
+        player.level, player.multikill, player.level_multikill)).send(
+            player.index)
+
+
+@Event
+def player_death(game_event):
+    """Award the killer if necessary."""
+    # Is GunGame active?
+    if gungame_status.match is not GunGameStatus.ACTIVE:
+        return
+
+    # Is the round active or should kills after the round count?
+    if (gungame_status.round is GunGameStatus.INACTIVE and
+            not ConVar('gg_count_after_round').get_int()):
+        return
+
+    # Get the victim
+    victim = game_event.get_int('userid')
+
+    # Get the attacker
+    attacker = game_event.get_int('attacker')
+
+    # Was this a suicide?
+    if attacker in (victim, 0):
+        return
+
+    # Get the victim's instance
+    vplayer = player_dictionary[victim]
+
+    # Get the attacker's instance
+    aplayer = player_dictionary[attacker]
+
+    # Was this a team-kill?
+    if vplayer.team == aplayer.team:
+        return
+
+    # Did the killer kill using their level's weapon?
+    if game_event.get_string('weapon') != aplayer.level_weapon:
+        return
+
+    # Increase the killer's multikill
+    aplayer.multikill += 1
+
+    # Does the player need leveled up?
+    if aplayer.multikill < aplayer.level_multikill:
+
+        # If not, no need to go further
+        return
+
+    # Level the player up
+    aplayer.increase_level(1, victim, 'kill')
+
+
+@Event
+def player_disconnect(game_event):
+    player_dictionary.safe_remove(game_event.get_int('userid'))
+
+
+@Event
+def round_start(game_event):
+    for entity in EntityIter('func_buyzone', return_types='entity'):
+        entity.disable()
+
+
+@Event
+def gg_win(game_event):
+    winner = player_dictionary[game_event.get_int('winner')]
+    if not winner.is_fake_client():
+        winner.wins += 1
+    for entity in EntityIter('game_end', return_types='entity'):
+        break
+    else:
+        entity = Entity.create('game_end')
+    entity.end_game()
+
+
+@LevelShutdown
+def level_shutdown():
+    player_dictionary.clear()
