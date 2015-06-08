43ca9ad84c9682f8a9f29301f43fd775c3fd17b6
Stephen Toon <satoon101@gmail.com>
2015-06-08 18:01:53 -0400

    plugins.valid no longer raises exceptions.  Changed the format of the description/info when getting values for sub-plugins.  This fixes #1.

--- addons/source-python/plugins/gungame/core/plugins/valid.py
+++ addons/source-python/plugins/gungame/core/plugins/valid.py
@@ -6,15 +6,13 @@
 # >> IMPORTS
 # =============================================================================
 from importlib import import_module
+from warnings import warn
 
 # Source.Python Imports
 #   Plugins
 from plugins.info import PluginInfo
 
 # GunGame Imports
-from gungame.core.plugins.errors import GGPluginFileNotFoundError
-from gungame.core.plugins.errors import GGPluginDescriptionMissingError
-from gungame.core.plugins.errors import GGPluginInfoMissingError
 from gungame.core.plugins.paths import GUNGAME_PLUGIN_PATH
 
 
@@ -60,7 +58,7 @@ class _ValidPlugins(object):
         for plugin_type in ('included', 'custom'):
             if plugin in getattr(self, plugin_type):
                 return plugin_type
-        raise ValueError('No such plugin "{0}"'.format(plugin))
+        raise ValueError('No such plugin "{0}".'.format(plugin))
 
     @property
     def included(self):
@@ -80,30 +78,44 @@ class _ValidPlugins(object):
     @staticmethod
     def _get_plugins_by_type(plugin_type):
         """Store each plugin for the given type."""
+        # Create a dictionary to store plugins by name
         plugins = {}
+
+        # Loop through all plugins
         for plugin in GUNGAME_PLUGIN_PATH.joinpath(plugin_type).dirs():
+
+            # Skip the compiled files
             if plugin.namebase == '__pycache__':
                 continue
+
+            # Does the primary file not exist?
             if not plugin.joinpath(plugin.namebase + '.py').isfile():
-                raise GGPluginFileNotFoundError()
-            module = 'gungame.plugins.{0}.{1}'.format(
-                plugin_type, plugin.namebase)
-            values = import_module(module)
-            if type(values.__path__).__name__ == '_NamespacePath':
+                warn()
                 continue
-            for name in values.__dict__:
-                if isinstance(values.__dict__[name], PluginInfo):
-                    info = values.__dict__[name]
-                    break
-            else:
-                raise GGPluginInfoMissingError()
-            if 'translations' in info and 'Description' in info.translations:
-                description = info.translations['Description']
-            elif values.__doc__:
-                description = values.__doc__
-            else:
-                raise GGPluginDescriptionMissingError()
-            plugins[str(plugin.namebase)] = ValidPlugin(info, description)
+
+            # Does the info file not exist?
+            if not plugin.joinpath('info.py').isfile():
+                warn()
+                continue
+
+            # Get the plugin's description
+            description = import_module('gungame.plugins.{0}.{1}'.format(
+                plugin_type, plugin.namebase)).__doc__
+
+            # Get the plugin's info
+            info = import_module('gungame.plugins.{0}.{1}.info'.format(
+                plugin_type, plugin.namebase))
+
+            # Does the info have an info attribute?
+            if not hasattr(info, 'info'):
+                warn()
+                continue
+
+            # Add the plugin to the dictionary
+            plugins[str(plugin.namebase)] = ValidPlugin(info.info, description)
+
+        # Return the dictionary
         return plugins
 
+# Get the _ValidPlugins instance
 valid_plugins = _ValidPlugins()
