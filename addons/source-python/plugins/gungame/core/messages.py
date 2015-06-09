4339bbe6673956e3ed2a4d0c834f85c6c21c7d37
Stephen Toon <satoon101@gmail.com>
2015-06-09 15:31:24 -0400

    Fixed calling messages.

--- addons/source-python/plugins/gungame/core/messages.py
+++ addons/source-python/plugins/gungame/core/messages.py
@@ -41,61 +41,63 @@ class _MessageManager(dict):
                     continue
                 self[key] = value
 
-    def center_message(self, users=None, message=''):
+    def center_message(self, users=None, message='', **tokens):
         """Send a center message to the given players."""
         message = self._get_message(message)
         instance = TextMsg(
-            destination=TextMsg.HUD_PRINTCENTER, message=message)
-        instance.send(users or ())
+            destination=TextMsg.HUD_PRINTCENTER, message=message, **tokens)
+        instance.send(*(users or ()))
 
-    def chat_message(self, users=None, index=0, message=''):
+    def chat_message(self, users=None, index=0, message='', **tokens):
         """Send a chat message to the given players."""
         message = self._get_message(message)
-        instance = SayText2(index=index, message=message)
-        instance.send(users or ())
+        instance = SayText2(index=index, message=message, **tokens)
+        instance.send(*(users or ()))
 
-    def echo_message(self, users=None, message=''):
+    def echo_message(self, users=None, message='', **tokens):
         """Send an echo message to the given players."""
         message = self._get_message(message)
         instance = TextMsg(
-            destination=TextMsg.HUD_PRINTCONSOLE, message=message)
-        instance.send(users or ())
+            destination=TextMsg.HUD_PRINTCONSOLE, message=message, **tokens)
+        instance.send(*(users or ()))
 
-    def hint_message(self, users=None, message=''):
+    def hint_message(self, users=None, message='', **tokens):
         """Send a hint message to the given players."""
         message = self._get_message(message)
-        instance = HintText(message=message)
-        instance.send(users or ())
+        instance = HintText(message=message, **tokens)
+        instance.send(*(users or ()))
 
     def hud_message(
             self, users=None, x=-1.0, y=-1.0, color1=WHITE,
             color2=WHITE, effect=0, fadein=0.0, fadeout=0.0,
-            hold=4.0, fxtime=0.0, message=''):
+            hold=4.0, fxtime=0.0, message='', **tokens):
         """Send a hud message to the given players."""
         message = self._get_message(message)
         instance = HudMsg(
             x=x, y=y, r1=color1.r, g1=color1.g, b1=color1.b, a1=color1.a,
             r2=color2.r, g2=color2.g, b2=color2.b, a2=color2.a,
             effect=effect, fadein=fadein, fadeout=fadeout,
-            hold=hold, fxtime=fxtime, message=message)
-        instance.send(users or ())
+            hold=hold, fxtime=fxtime, message=message, **tokens)
+        instance.send(*(users or ()))
 
-    def keyhint_message(self, users=None, message=''):
+    def keyhint_message(self, users=None, message='', **tokens):
         """Send a keyhint message to the given players."""
         message = self._get_message(message)
-        instance = KeyHintText(message=message)
-        instance.send(users or ())
+        instance = KeyHintText(message=message, **tokens)
+        instance.send(*(users or ()))
 
     def motd_message(
             self, users=None, panel_type=2, title='',
-            message='', visible=True):
+            message='', visible=True, **tokens):
         """Send a motd message to the given players."""
         message = self._get_message(message)
         subkeys = {'title': title, 'type': panel_type, 'msg': message}
-        instance = VGUIMenu(name='info', show=visible, subkeys=subkeys)
-        instance.send(users or ())
+        instance = VGUIMenu(
+            name='info', show=visible, subkeys=subkeys, **tokens)
+        instance.send(*(users or ()))
 
-    def top_message(self, users=None, message='', color=WHITE, time=4.0):
+    def top_message(
+            self, users=None, message='', color=WHITE, time=4.0, **tokens):
         """Send a toptext message to the given players."""
         # message = self._get_message(message)
         raise NotImplementedError(
