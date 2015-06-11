f28d6bde1b99ee32e26e2aecaa57fca69d1e828e
Stephen Toon <satoon101@gmail.com>
2015-06-10 21:54:31 -0400

    Started implementation of sound system.

---
+++ addons/source-python/plugins/gungame/core/sounds.py
@@ -0,0 +1,26 @@
+# ../gungame/core/sounds.py
+
+"""Sound based functionality."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class _SoundManager(object):
+
+    """Class used to play sounds."""
+
+    def play_sound(self, sound, users=None):
+        """Play the sound to the given users."""
+
+    def emit_sound(self, sound, index, users=None):
+        """Emit the sound from the given index and play to the given users."""
+
+    def stop_sound(self, sound, index):
+        """Stop the sound from playing from the given index."""
+
+sound_manager = _SoundManager()
