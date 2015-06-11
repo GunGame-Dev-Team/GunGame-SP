08d3c6a78048c2e510c1f9706964972420ea100c
Stephen Toon <satoon101@gmail.com>
2015-06-10 20:19:11 -0400

    Implemented sub-plugin custom event registration system.
    Plugins need to have a custom-event.py file in their main directory that houses the custom events.

--- addons/source-python/plugins/gungame/core/events/__init__.py
+++ addons/source-python/plugins/gungame/core/events/__init__.py
@@ -13,8 +13,21 @@ from importlib import import_module
 #   Path
 from path import Path
 
+# GunGame Imports
+#   Plugins
+from gungame.core.plugins.paths import GUNGAME_PLUGIN_PATH
+from gungame.core.plugins.valid import valid_plugins
+
 
 # Import the included events
 for event_file in Path(__file__).parent.joinpath('included').files():
     if event_file.namebase != '__init__':
         import_module('gungame.core.events.included.' + event_file.namebase)
+
+for plugin_name in valid_plugins.all:
+    plugin_type = valid_plugins.get_plugin_type(plugin_name)
+    if GUNGAME_PLUGIN_PATH.joinpath(
+            plugin_type, plugin_name, 'custom_events.py').isfile():
+        import_module(
+            'gungame.plugins.{0}.{1}.custom_events'.format(
+                plugin_type, plugin_name))
