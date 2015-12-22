da89fceb9f28fe0e13d76ad9cc43217d5e298348
Stephen Toon <satoon101@gmail.com>
2015-12-22 12:41:32 -0500

    Updated gg_dissolver to use configuration properly.

---
+++ addons/source-python/plugins/gungame/plugins/included/gg_dissolver/configuration.py
@@ -0,0 +1,39 @@
+# ../gungame/plugins/included/gg_dissolver/configuration.py
+
+"""Creates the gg_disable_objectives configuration."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Cvars
+from cvars.flags import ConVarFlags
+
+# GunGame Imports
+#   Config
+from gungame.core.config.manager import GunGameConfigManager
+
+# Script Imports
+from .info import info
+
+
+# =============================================================================
+# >> CONFIGURATION
+# =============================================================================
+with GunGameConfigManager(info.name) as config:
+    with config.cvar(
+            'type', 0, ConVarFlags.NONE,
+            'Set to the type of dissolver to use.') as dissolver_type:
+        for _name in DissolveType.__members__:
+            dissolver_type.Options.append('{0} = {1}'.format(
+                getattr(DissolveType, _name).real, _name)
+        _num_dissolve_types = len(DissolveType)
+        dissolver_type.Options.append(
+            '{0} = RANDOM'.format(_num_dissolve_types))
+        dissolver_type.Options.append(
+            '{0} = REMOVE'.format(_num_dissolve_types + 1))
+
+    with config.cvar(
+            'magnitude', 2, ConVarFlags.NONE,
+            'Set to the magnitude to use when dissolving') as magnitude:
+        ...
--- addons/source-python/plugins/gungame/plugins/included/gg_dissolver/gg_dissolver.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_dissolver/gg_dissolver.py
@@ -1,15 +1,17 @@
-from cvars import ConVar
 from entities.entity import Entity
 from events import Event
+
+from .configuration import dissolver_type
+from .configuration import magnitude
 
 
 @Event('player_death')
 def player_death(game_event):
     """"""
     entity = Entity.find_or_create('env_entity_dissolver')
-    dissolve_type = ConVar('gg_dissolver_type').get_int()
-    if dissolve_type not in range(4):
-        dissolve_type = randint(0, 3)
-    entity.dissolve_type = dissolve_type
-    entity.magnitude = ConVar('gg_dissolver_magnitude').get_int()
+    current = dissolve_type.get_int()
+    if current not in range(4):
+        current = randint(0, 3)
+    entity.dissolve_type = current
+    entity.magnitude = magnitude.get_int()
     entity.dissolve('cs_ragdoll')
