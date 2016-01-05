0963b8115b8ec953e028eb633172ceb948f00463
Stephen Toon <satoon101@gmail.com>
2016-01-05 11:22:52 -0500

    Added translations for the menus.

--- addons/source-python/plugins/gungame/core/menus/__init__.py
+++ addons/source-python/plugins/gungame/core/menus/__init__.py
@@ -1,3 +1,20 @@
 # ../gungame/core/menus/__init__.py
 
 """GunGame menus functionality."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Translations
+from translations.strings import LangStrings
+
+# GunGame Imports
+#   Paths
+from gungame.core.paths import GUNGAME_TRANSLATION_PATH
+
+
+# =============================================================================
+# >> GLOBAL VARIABLES
+# =============================================================================
+_menu_strings = LangStrings(GUNGAME_TRANSLATION_PATH / 'menus')
