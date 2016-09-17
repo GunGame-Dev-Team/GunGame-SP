2170b769efeb9f1a0422bd56e3ce5092f405b00d
Stephen Toon <satoon101@gmail.com>
2016-09-17 16:04:53 -0400

    Fixed issues in rank/winners commands.

--- addons/source-python/plugins/gungame/core/menus/rank.py
+++ addons/source-python/plugins/gungame/core/menus/rank.py
@@ -27,6 +27,6 @@ def _get_player_page(menu, player):
     """Get the page that the player is listed on."""
     for page_number in range(menu.page_count + 1):
         for option in menu._get_options(page_number):
-            if option.value == player.unique_id:
+            if option.value == player.uniqueid:
                 return page_number
     return 1
