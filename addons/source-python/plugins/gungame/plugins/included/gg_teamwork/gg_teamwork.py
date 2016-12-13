034a0c204a2451a54b77715bae63905a1ac0be56
Stephen Toon <satoon101@gmail.com>
2016-12-13 18:35:29 -0500

    Fully implemented teamplay.

--- addons/source-python/plugins/gungame/plugins/included/gg_teamwork/gg_teamwork.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_teamwork/gg_teamwork.py
@@ -6,17 +6,26 @@
 # >> IMPORTS
 # =============================================================================
 # Source.Python
-from colors import BLUE, RED
+from colors import BLUE, RED, WHITE
+from cvars import ConVar
 from entities.entity import Entity
 from events import Event
 from events.hooks import EventAction, PreEvent
 from listeners import OnLevelInit
+from listeners.tick import Delay
 from players.teams import teams_by_number
 
 # GunGame
+from gungame.core.config.misc import dynamic_chat_time
+from gungame.core.messages import message_manager
 from gungame.core.players.dictionary import player_dictionary
+from gungame.core.sounds.manager import sound_manager
+from gungame.core.status import GunGameMatchStatus, GunGameStatus
+from gungame.core.teams import team_names
+from gungame.core.weapons.manager import weapon_order_manager
 
 # Plugin
+from .configuration import join_team_level
 from .custom_events import GG_Team_Win
 
 
@@ -30,49 +39,112 @@ class _TeamManager(dict):
 
 
 class _TeamManagement(object):
-    level = 1
-    leader = None
 
     def __init__(self, number, alias):
         self.number = number
         self.alias = alias
+        self.level = 1
+        self._name = None
+        self._leader = None
+        self.leader_userid = None
 
     @property
     def index(self):
-        return 1
+        return self.leader.level if self.leader is not None else 0
+
+    @property
+    def name(self):
+        if self._name is None:
+            self._name = team_names[self.number]
+        return self._name
+
+    @property
+    def leader(self):
+        return self._leader
+
+    @leader.setter
+    def leader(self, player):
+        current = self._leader
+        self._leader = player
+        self.leader_userid = None if player is None else player.userid
+        if current is None and player is None:
+            return
+        if current.level < player.level:
+            message_manager.chat_message(
+                message='TeamWork:Leader:Increase',
+                index=self.index,
+                player=player.name,
+                team_name=self.name,
+                level=self.leader_level,
+            )
 
     @property
     def leader_level(self):
-        return 1
+        return None if self.leader is None else self.leader.level
 
     @property
     def color(self):
         return RED if self.number == 2 else BLUE
 
     def reset_values(self):
-        self.level = 1
         self.leader = None
 
-    def set_all_player_levels(self):
-        pass
+    def set_team_player_levels(self):
+        self.level = self.leader_level
+        if self.level is None:
+            return
+        for player in player_dictionary:
+            if player.team != self.number:
+                continue
+            if player.level == self.level:
+                continue
+            player.level = self.level
+            player.multi_kill = 0
 
-    def set_joining_player_level(self):
-        pass
+    def set_joining_player_level(self, player):
+        player.level = self.level if join_team_level.get_int() else 1
 
-    def check_old_leader(self):
-        pass
+    def find_team_leader(self, leveler=None, old_level=None, disconnect=False):
+        team_players = {
+            player.level: player for player in player_dictionary
+            if player.team == self.number
+        }
+        if not team_players:
+            self.leader = None
+            return
+        self.leader = team_players[max(team_players.keys())]
 
-    def find_team_leader(self):
-        pass
+        if disconnect:
+            message_manager.chat_message(
+                message='TeamWork:Leader:Disconnect',
+                index=self.index,
+                team_name=self.name,
+                level=self.leader_level,
+            )
+            return
+
+        if leveler is None or old_level is None:
+            return
+
+        if self.leader_level is not None and self.leader_level < old_level:
+            message_manager.chat_message(
+                message='TeamWork:Leader:Decrease',
+                index=self.index,
+                player=leveler.name,
+                team_name=self.name,
+                level=self.leader_level,
+            )
 
     def send_level_message(self):
-        pass
-
-    def send_winner_messages(self):
-        pass
-
-    def send_message_to_all(self, message, **tokens):
-        pass
+        if self.leader_level is None:
+            return
+        message_manager.chat_message(
+            message='TeamWork:TeamLevel',
+            index=self.index,
+            team_name=self.name,
+            level=self.leader_level,
+            weapon=weapon_order_manager.active[self.leader_level].weapon,
+        )
 
 _team_manager = _TeamManager({
     number: _TeamManagement(number, alias)
@@ -84,38 +156,106 @@ _team_manager = _TeamManager({
 # =============================================================================
 # >> GAME EVENTS
 # =============================================================================
-@Event('')
-def something():
-    pass
+@Event('player_team')
+def _check_team_leaders(game_event):
+    userid = game_event['userid']
+    old_team_number = game_event['oldteam']
+
+    old_team = _team_manager.get(old_team_number)
+    if old_team and old_team.leader_userid == userid:
+        old_team.find_team_leader(disconnect=True)
+
+    new_team = _team_manager.get(game_event['team'])
+    if not new_team:
+        return
+
+    if not old_team_number:
+        new_team.set_joining_player_level(player_dictionary[userid])
+
+    else:
+        new_team.find_team_leader()
 
 
-@Event('gg_start', 'gg_team_win')
-@OnLevelInit
-def _clear_team_dictionary(game_event=None):
-    # TODO: clear team dictionary
-    pass
+@Event('round_start')
+def _send_level_messages(game_event):
+    for team in _team_manager.values():
+        team.send_level_message()
+
+
+@Event('round_end')
+def _sync_player_levels(game_event):
+    for team in _team_manager.values():
+        team.set_team_player_levels()
 
 
 # =============================================================================
 # >> GUNGAME EVENTS
 # =============================================================================
-@Event('gg_team_win')
-def _end_match(game_event):
-    # TODO: send winner messages
-    # TODO: play winner sound
-    Entity.find_or_create('game_end').end_game()
+@Event('gg_start')
+@OnLevelInit
+def _clear_team_dictionary(game_event=None):
+    _team_manager.clear()
 
 
 @Event('gg_level_up')
-def _check_team_increase(game_event):
-    # TODO: check to see if player increased team's level
+def _level_up(game_event):
     player = player_dictionary[game_event['leveler']]
+    team = _team_manager[player.team]
+    if team.leader is None or team.leader_level < game_event['new_level']:
+        team.leader = player
 
 
 @Event('gg_level_down')
 def _check_team_decrease(game_event):
-    # TODO: check to see if player decreased team's level
     player = player_dictionary[game_event['leveler']]
+    team = _team_manager[player.team]
+    if team.leader.userid == player.userid:
+        team.find_team_leader(player, game_event['old_level'])
+
+
+@Event('gg_team_win')
+def _end_match(game_event):
+    # Set the match status
+    GunGameStatus.MATCH = GunGameMatchStatus.POST
+
+    # Get the winning team information
+    winning_team = game_event['winner']
+
+    # Send the winner messages
+    message_manager.chat_message(
+        index=winning_team.index,
+        message='TeamWork:Winner:Long',
+        team_name=winning_team.name,
+    )
+    for second in range(4):
+        Delay(
+            second,
+            message_manager.center_message,
+            kwargs={
+                'message': 'TeamWork:Winner:Short',
+                'team_name': winning_team.name,
+            }
+        )
+    color = {2: RED, 3: BLUE}.get(winning_team, WHITE)
+    message_manager.top_message(
+        message='TeamWork:Winner:Short',
+        color=color,
+        team_name=winning_team.name,
+    )
+
+    # Play the winner sound
+    winner_sound = sound_manager.play_sound('winner')
+
+    # Set the dynamic chat time, if needed
+    if dynamic_chat_time.get_bool():
+        ConVar('mp_chattime').set_float(winner_sound.duration)
+
+    # End the match to move to the next map
+    entity = Entity.find_or_create('game_end')
+    entity.end_game()
+
+    # Reset the teams
+    _clear_team_dictionary()
 
 
 # =============================================================================
