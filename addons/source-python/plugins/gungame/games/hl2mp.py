84c44e23cdc490868778bda669b2ff27e10cb904
Stephen Toon <satoon101@gmail.com>
2016-12-12 22:23:32 -0500

    Added weapon_fire event calling to both BMS and HL2:DM.

---
+++ addons/source-python/plugins/gungame/games/hl2mp.py
@@ -0,0 +1,48 @@
+# ../gungame/games/hl2mp.py
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+from entities.entity import Entity
+from events.custom import CustomEvent
+from events.variable import ShortVariable, StringVariable
+from events.resource import ResourceFile
+from listeners import OnEntitySpawned
+from players.entity import Player
+
+
+# =============================================================================
+# >> CUSTOM EVENTS
+# =============================================================================
+class Weapon_Fire(CustomEvent):
+    userid = ShortVariable('The userid of the player that fired the weapon.')
+    weapon = StringVariable('The type of weapon that was fired.')
+
+resource_file = ResourceFile('gungame_hl2mp', Weapon_Fire)
+resource_file.write()
+resource_file.load_events()
+
+
+# =============================================================================
+# >> LISTENERS
+# =============================================================================
+@OnEntitySpawned
+def entity_spawned(base_entity):
+    try:
+        entity = Entity(base_entity.index)
+    # TODO: clarify this exception
+    except Exception:
+        return
+
+    if entity.classname != 'npc_grenade_frag':
+        return
+
+    try:
+        player = Player(entity.owner.index)
+    # TODO: clarify this exception
+    except Exception:
+        return
+
+    with Weapon_Fire() as event:
+        event.userid = player.userid
+        event.weapon = 'frag'
