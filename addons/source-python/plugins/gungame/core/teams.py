7d6481c84b030433b71c4388b55e7af3a7766c50
Stephen Toon <satoon101@gmail.com>
2017-01-02 10:21:21 -0500

    Fixed team_levels/team_names population when loaded via autoexec.

--- addons/source-python/plugins/gungame/core/teams.py
+++ addons/source-python/plugins/gungame/core/teams.py
@@ -28,15 +28,34 @@ class _TeamLevels(dict):
             self[x] = 0
 
 
+class _TeamNames(dict):
+    def __missing__(self, key):
+        if teams_by_number.get(key, 'un') in ('un', 'spec'):
+            raise ValueError(
+                'Invalid team number "{key}".'.format(
+                    key=key,
+                )
+            )
+
+        value = None
+        for _class_name in team_managers:
+            for _entity in EntityIter(_class_name):
+                if _entity.team == key:
+                    value = self[key] = _entity.team_name
+                    team_levels[key] = 0
+                    break
+            if value is not None:
+                break
+        else:
+            raise LookupError(
+                'Entity not found for team "{key}".'.format(
+                    key=key,
+                )
+            )
+
+
 # =============================================================================
 # >> GLOBAL VARIABLES
 # =============================================================================
-team_names = dict()
+team_names = _TeamNames()
 team_levels = _TeamLevels()
-
-for _class_name in team_managers:
-    for _entity in EntityIter(_class_name):
-        if teams_by_number.get(_entity.team, 'un') in ('un', 'spec'):
-            continue
-        team_names[_entity.team] = _entity.team_name
-        team_levels[_entity.team] = 0
