e0e19b0082545848a631d1ee82f455f1d5b61371
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:36:50 -0500

    Updated deathmatch rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_deathmatch/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_deathmatch/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .configuration import delay
@@ -18,19 +17,9 @@ from .info import info
 # >> RULES
 # =============================================================================
 deathmatch_rules = GunGameRules(info.name)
-deathmatch_rules.title = 'DeathmatchRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('DeathmatchRules:'):
-        deathmatch_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
-
-for _token_name, _convar in (
-    ('delay', delay),
-):
-    deathmatch_rules.register_convar_token(
-        token_name=_token_name,
-        convar=_convar,
-        convar_type='float',
-    )
+deathmatch_rules.register_convar_token(
+    token_name='delay',
+    convar=delay,
+    convar_type='float',
+)
+deathmatch_rules.register_all_rules()
