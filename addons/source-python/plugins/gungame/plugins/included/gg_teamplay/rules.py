6e83aa48aa3341efbffa383aff6e739f79bf782b
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:41:17 -0500

    Updated teamplay rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_teamplay/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_teamplay/rules.py
@@ -20,21 +20,21 @@ from .info import info
 # >> RULES
 # =============================================================================
 teamplay_rules = GunGameRules(info.name)
-teamplay_rules.title = 'TeamplayRules'
+teamplay_rules.register_convar_token(
+    token_name='count_grenade_kills',
+    convar=count_grenade_kills,
+)
+teamplay_rules.register_convar_token(
+    token_name='count_melee_kills',
+    convar=count_melee_kills,
+)
+teamplay_rules.register_convar_token(
+    token_name='end_on_first_kill',
+    convar=end_on_first_kill,
+)
 for _key, _value in rules_translations.items():
-    if _key.startswith('TeamplayRules:Base:'):
+    if _key.startswith('{title}:Base:'.format(title=info.name)):
         teamplay_rules.register_rule(
             name=_key,
             value=_value,
         )
-
-for _token_name, _convar in (
-    ('count_grenade_kills', count_grenade_kills),
-    ('count_melee_kills', count_melee_kills),
-    ('end_on_first_kill', end_on_first_kill),
-):
-    teamplay_rules.register_convar_token(
-        token_name=_token_name,
-        convar=_convar,
-        convar_type='int',
-    )
