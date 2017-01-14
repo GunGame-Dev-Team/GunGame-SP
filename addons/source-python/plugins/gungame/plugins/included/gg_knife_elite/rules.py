e27e411c7e8be6a6944b082231a2706909407ea0
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:40:05 -0500

    Updated knife elite rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_knife_elite/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_knife_elite/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 knife_elite_rules = GunGameRules(info.name)
-knife_elite_rules.title = 'KnifeEliteRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('KnifeEliteRules:'):
-        knife_elite_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+knife_elite_rules.register_all_rules()
