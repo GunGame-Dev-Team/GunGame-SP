5c260447f38d4efb4b1a6422091c8079cc984233
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:38:48 -0500

    Updated elimination rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_elimination/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_elimination/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 elimination_rules = GunGameRules(info.name)
-elimination_rules.title = 'EliminationRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('EliminationRules:'):
-        elimination_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+elimination_rules.register_all_rules()
