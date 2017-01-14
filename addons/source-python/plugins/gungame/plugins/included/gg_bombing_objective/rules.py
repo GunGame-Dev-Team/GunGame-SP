f54e9e79463aafe80417200362c21265d2003926
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:37:30 -0500

    Updated bombing objective rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_bombing_objective/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_bombing_objective/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .configuration import defused_levels, detonated_levels
@@ -18,20 +17,12 @@ from .info import info
 # >> RULES
 # =============================================================================
 bombing_objective_rules = GunGameRules(info.name)
-bombing_objective_rules.title = 'BombingRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('BombingRules:'):
-        bombing_objective_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
-
-for _token_name, _convar in (
-    ('defused_levels', defused_levels),
-    ('detonated_levels', detonated_levels),
-):
-    bombing_objective_rules.register_convar_token(
-        token_name=_token_name,
-        convar=_convar,
-        convar_type='int',
-    )
+bombing_objective_rules.register_convar_token(
+    token_name='defused_levels',
+    convar=defused_levels,
+)
+bombing_objective_rules.register_convar_token(
+    token_name='detonated_levels',
+    convar=detonated_levels,
+)
+bombing_objective_rules.register_all_rules()
