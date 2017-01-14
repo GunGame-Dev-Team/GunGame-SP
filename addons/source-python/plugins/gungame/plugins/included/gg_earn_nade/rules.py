bc8703642af3f7fb31ced940ea638ffeaa07fa6b
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:38:18 -0500

    Updated earn nade rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_earn_nade/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_earn_nade/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 earn_nade_rules = GunGameRules(info.name)
-earn_nade_rules.title = 'EarnNadeRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('EarnNadeRules:'):
-        earn_nade_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+earn_nade_rules.register_all_rules()
