a6ed9b575b2df03d1309f008f5f28d33c7c460ca
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:41:33 -0500

    Updated teamwork rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_teamwork/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_teamwork/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 teamwork_rules = GunGameRules(info.name)
-teamwork_rules.title = 'TeamworkRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('TeamworkRules:'):
-        teamwork_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+teamwork_rules.register_all_rules()
