64b652e124948b6f353de8f931ac9008a9cb6b0f
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:39:13 -0500

    Updated ffa rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_ffa/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_ffa/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 ffa_rules = GunGameRules(info.name)
-ffa_rules.title = 'FFARules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('FFARules:'):
-        ffa_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+ffa_rules.register_all_rules()
