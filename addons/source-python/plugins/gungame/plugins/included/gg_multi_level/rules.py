d04220735add383fbc4500b15dfccc7a233fb5e3
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:40:51 -0500

    Updated multi level rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_multi_level/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_multi_level/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .configuration import length, levels
@@ -18,20 +17,12 @@ from .info import info
 # >> RULES
 # =============================================================================
 multi_level_rules = GunGameRules(info.name)
-multi_level_rules.title = 'MultiLevelRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('MultiLevelRules:'):
-        multi_level_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
-
-for _token_name, _convar in (
-    ('length', length),
-    ('levels', levels),
-):
-    multi_level_rules.register_convar_token(
-        token_name=_token_name,
-        convar=_convar,
-        convar_type='int',
-    )
+multi_level_rules.register_convar_token(
+    token_name='length',
+    convar=length,
+)
+multi_level_rules.register_convar_token(
+    token_name='levels',
+    convar=levels,
+)
+multi_level_rules.register_all_rules()
