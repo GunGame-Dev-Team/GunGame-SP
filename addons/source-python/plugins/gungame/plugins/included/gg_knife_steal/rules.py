3c54417bf0022c2f1e32de032355716540f4cf80
Stephen Toon <satoon101@gmail.com>
2017-01-13 20:40:24 -0500

    Updated knife steal rules to use new method.

--- addons/source-python/plugins/gungame/plugins/included/gg_knife_steal/rules.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_knife_steal/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,10 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 knife_steal_rules = GunGameRules(info.name)
-knife_steal_rules.title = 'KnifeStealRules'
-for _key, _value in rules_translations.items():
-    if _key.startswith('KnifeStealRules:'):
-        knife_steal_rules.register_rule(
-            name=_key,
-            value=_value,
-        )
+knife_steal_rules.register_all_rules()
