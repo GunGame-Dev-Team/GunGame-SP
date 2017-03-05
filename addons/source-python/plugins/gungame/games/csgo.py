44d8c923acb66745c336090ee88dc3d8f6bc5d26
Stephen Toon <satoon101@gmail.com>
2017-03-04 19:33:53 -0500

    Fixed molotov/incgrenade kills for CS:GO counting towards level.

--- addons/source-python/plugins/gungame/games/csgo.py
+++ addons/source-python/plugins/gungame/games/csgo.py
@@ -10,9 +10,17 @@ from warnings import warn
 
 # Source.Python
 from core import GAME_NAME
+from events import Event
+from weapons.manager import weapon_manager
 
 # GunGame
+from gungame.core.config.misc import allow_kills_after_round, level_on_protect
 from gungame.core.messages import message_manager
+from gungame.core.players.dictionary import player_dictionary
+from gungame.core.status import (
+    GunGameMatchStatus, GunGameRoundStatus, GunGameStatus,
+)
+from gungame.core.weapons.groups import incendiary_weapons
 
 
 # =============================================================================
@@ -39,3 +47,72 @@ class _NoMessage(object):
 message_manager.hud_message = _NoMessage('HudMsg')._message_hook
 message_manager.top_message = _NoMessage('DialogMsg')._message_hook
 message_manager.center_message = message_manager.hint_message
+
+
+@Event('player_death')
+def _player_death(game_event):
+    """Award the killer with a multi-kill increase or level increase."""
+    # Is GunGame active?
+    if GunGameStatus.MATCH is not GunGameMatchStatus.ACTIVE:
+        return
+
+    # Is the round active or should kills after the round count?
+    if (
+        GunGameStatus.ROUND is GunGameRoundStatus.INACTIVE and
+        not allow_kills_after_round.get_int()
+    ):
+        return
+
+    # Get the victim
+    userid = game_event['userid']
+
+    # Get the attacker
+    attacker = game_event['attacker']
+
+    # Was this a suicide?
+    if attacker in (userid, 0):
+        return
+
+    # Get the victim's instance
+    victim = player_dictionary[userid]
+
+    # Get the attacker's instance
+    killer = player_dictionary[attacker]
+
+    # Was this a team-kill?
+    if victim.team == killer.team:
+        return
+
+    if killer.in_spawn_protection and not level_on_protect.get_int():
+        return
+
+    # Did the killer kill using their level's weapon?
+    try:
+        weapon = weapon_manager[game_event['weapon']].basename
+    except:
+        return
+
+    if weapon != 'molotov':
+        return
+
+    if killer.level_weapon in incendiary_weapons:
+        weapon = killer.level_weapon
+
+    if weapon_manager[weapon].basename != killer.level_weapon:
+        return
+
+    # Increase the killer's multi_kill
+    killer.multi_kill += 1
+
+    # Does the player need leveled up?
+    if killer.multi_kill < killer.level_multi_kill:
+
+        # If not, no need to go further
+        return
+
+    # Level the player up
+    killer.increase_level(
+        levels=1,
+        reason='kill',
+        victim=userid,
+    )
