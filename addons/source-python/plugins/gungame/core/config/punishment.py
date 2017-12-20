e88e5e93ab4a9ffacfdd92624f9a1f3613b68880
Stephen Toon <satoon101@gmail.com>
2017-12-20 18:03:35 -0500

    Implemented AFK punishment.

--- addons/source-python/plugins/gungame/core/config/punishment.py
+++ addons/source-python/plugins/gungame/core/config/punishment.py
@@ -26,22 +26,20 @@ __all__ = (
 # >> CONFIGURATION
 # =============================================================================
 with GunGameConfigManager('punishment') as _config:
-    _config._cvar_prefix = 'gg_'
 
     # AFK
-    # TODO: add in functionality
-    # with _config.cvar(
-    #     name='afk',
-    # ) as afk_punish:
-    #     afk_punish.add_text()
-    # with _config.cvar(
-    #     name='afk_type',
-    # ) as afk_type:
-    #     afk_type.add_text()
-    # with _config.cvar(
-    #     name='afk_length',
-    # ) as afk_length:
-    #     afk_length.add_text()
+    with _config.cvar(
+        name='afk',
+    ) as afk_punish:
+        afk_punish.add_text()
+    with _config.cvar(
+        name='afk_type',
+    ) as afk_type:
+        afk_type.add_text()
+    with _config.cvar(
+        name='afk_length',
+    ) as afk_length:
+        afk_length.add_text()
 
     # Suicide
     with _config.cvar(
