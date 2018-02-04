3dbea1212152d6beaa7a52573933ac4cc1368bad
satoon101 <satoon101@gmail.com>
2018-02-03 23:13:52 -0500

    Fixed random_spawn.

--- addons/source-python/plugins/gungame/plugins/included/gg_random_spawn/locations.py
+++ addons/source-python/plugins/gungame/plugins/included/gg_random_spawn/locations.py
@@ -26,14 +26,14 @@ __all__ = (
 def get_locations(class_name):
     """Return all current spawn points for the given class name."""
     for base_entity in BaseEntityIter(class_name):
-        yield base_entity.origin, base_entity.angle
+        yield base_entity.origin, base_entity.angles
 
 
 def set_location(class_name, origin, angles):
     """Create a spawn point at the given location."""
     base_entity = BaseEntity.create(class_name)
     base_entity.origin = origin
-    base_entity.angle = angles
+    base_entity.angles = angles
 
 
 def remove_locations(class_name):
